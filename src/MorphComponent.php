9ea8cf76db7959190dcbbcbafa11d480ddd6efc8
Andrew Rhyand <andrew.rhyand@gmail.com>
2022-10-25 12:08:01 -0600

    wip

---
+++ src/MorphComponent.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace BoxyBird\Morph;
+
+use Exception;
+use Illuminate\Encryption\Encrypter;
+use Symfony\Component\HttpFoundation\Request;
+
+class MorphComponent
+{
+    public $name;
+    
+    public $path;
+    
+    public $hash;
+    
+    public $event;
+
+    public $request;
+    
+    public $initial_data;
+    
+    public function __construct(string $name, array $initial_data = [])
+    {
+        $this->name = $name;
+        $this->initial_data = $initial_data;
+
+        $this->setPath();
+        $this->setHash();
+        $this->setRequest();
+        $this->setEvent();
+    }
+
+    protected function setPath(): void
+    {
+        $base_path = apply_filters('morph/component/path', get_template_directory() . '/morph/components/');
+        
+        $path = $base_path . $this->name . '.php';
+
+        if (!file_exists($path)) {
+            throw new Exception('Morph component not found: ' . $path);
+        }
+
+        $this->path = $path;
+    }
+
+    protected function setRequest(): void
+    {
+        $this->request = Request::createFromGlobals();
+    }
+
+    protected function setEvent(): void
+    {
+        $post = $this->request->request->all();
+
+        $event = [];
+
+        if (isset($post['wpMorphEvent']) && $post['wpMorphEvent'] === 'true') {
+            $event = [
+                'component' => $post['componentName'],
+                'data'      => $post['data'],
+            ];
+
+            unset($post['wpMorphEvent']);
+            unset($post['componentName']);
+            unset($post['data']);
+        }
+
+        $this->event = $event;
+    }
+
+    private function setHash(): void
+    {
+        $this->hash = (new Encrypter(BB_MORPH_HASH_KEY))->encrypt([
+            'initial_data'    => $this->initial_data,
+            'morph_data'      => [
+                'current_post_id' => get_the_ID(),
+                'nonce'           => wp_create_nonce('morph_ajax_nonce'),
+            ]
+        ]);
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -1,48 +1,21 @@
 <?php
 
-use Illuminate\Encryption\Encrypter;
-use Symfony\Component\HttpFoundation\Request;
+use BoxyBird\Morph\MorphComponent;
 
 if (!function_exists('morph_component')) {
     function morph_component(string $component_name, array $initial_data = []): void
     {
-        $base_path = apply_filters('morph/component/path', get_template_directory() . '/morph/components/');
-        
-        $component_path = $base_path . $component_name . '.php';
+        $component = new MorphComponent($component_name, $initial_data);
 
-        if (!file_exists($component_path)) {
-            throw new Exception('Morph component not found: ' . $component_path);
-        }
-
-        $hash = (new Encrypter(BB_MORPH_HASH_KEY))->encrypt([
-            'initial_data'    => $initial_data,
-            'morph_data'      => [
-                'current_post_id' => get_the_ID(),
-                'nonce'           => wp_create_nonce('morph_ajax_nonce'),
-            ]
-        ]);
-
-        $morph_request = Request::createFromGlobals();
-        $morph_files = $morph_request->files->all();
-        $morph_post = $morph_request->request->all();
-
-        // Prepare the component data if wpMorphEvent is set
-        $morph_event = [];
-        if (isset($morph_post['wpMorphEvent']) && $morph_post['wpMorphEvent'] === 'true') {
-            $morph_event = [
-                'component' => $morph_post['componentName'],
-                'data'      => $morph_post['data'],
-            ];
-
-            unset($morph_post['wpMorphEvent']);
-            unset($morph_post['componentName']);
-            unset($morph_post['data']);
-        }
+        $morph_request = $component->request;
+        $morph_files = $component->request->files->all();
+        $morph_post = $component->request->request->all();
+        $morph_event = $component->event;
         ?>
         <div 
-            data-wpmorph-component-hash="<?= $hash; ?>"
-            data-wpmorph-component-name="<?= $component_name; ?>">
-            <?php require $component_path; ?>
+            data-wpmorph-component-hash="<?= $component->hash; ?>"
+            data-wpmorph-component-name="<?= $component->name; ?>">
+            <?php require $component->path; ?>
         </div>
         <?php
     }
