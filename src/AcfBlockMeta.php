657f71a2c5056e991c27adca6fc8fb579f3bb5b7
Andrew Rhyand <andrew.rhyand@gmail.com>
2023-08-31 19:49:21 -0600

    Recursively filter out all ACF blocks

--- src/AcfBlockMeta.php
+++ src/AcfBlockMeta.php
@@ -12,7 +12,9 @@ class AcfBlockMeta
 
         $blocks = parse_blocks(get_the_content());
 
-        foreach ($blocks as $block) {
+        $acf_blocks = static::filterOutOnlyAcfBlocks($blocks);
+
+        foreach ($acf_blocks as $block) {
             $block_id_to_match = 'block_' . acf_get_block_id($block['attrs'], [
                 'postId'   => get_the_ID(),
                 'postType' => get_post_type(),
@@ -33,5 +35,22 @@ class AcfBlockMeta
         }
 
         acf_reset_meta($block_id);
+    }
+
+    protected static function filterOutOnlyAcfBlocks(array $blocks): array
+    {
+        $result = [];
+
+        foreach ($blocks as $block) {
+            if (isset($block['innerBlocks']) && is_array($block['innerBlocks'])) {
+                $result = array_merge($result, $block['innerBlocks'], static::filterOutOnlyAcfBlocks($block['innerBlocks']));
+            }
+        }
+
+        $result = array_filter($result, function ($block) {
+            return strpos($block['blockName'], 'acf/') === 0;
+        });
+
+        return array_values($result);
     }
 }
